33adb60ba0250908c54d6cf1178e759ebfb372b0
matybarta <matybarta@email.cz>
2023-03-30 10:00:25 +0200

    modified:   "Python/v\304\233ci/obesenec/obeseni.py"

--- Python/věci/obesenec/obeseni.py
+++ Python/věci/obesenec/obeseni.py
@@ -6,7 +6,3 @@ with open("./czech-words.txt", "r", encoding="utf8") as file:
     slova = file.read().splitlines()
 
 slovo = print(random.choice(slova))
-
-
-
-pismeno = input("Jaké písmeno hádáš: ")
