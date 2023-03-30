a7b8c217216bc38ebd5936111d23004356a57216
matybarta <matybarta@email.cz>
2023-03-30 09:54:03 +0200

    renamed:    "Python/v\304\233ci/obesenec/obesit.py" -> "Python/v\304\233ci/obesenec/obeseni.py"

--- Python/věci/obesenec/obeseni.py
+++ Python/věci/obesenec/obeseni.py
@@ -1,6 +1,12 @@
 import random
+
+list_pis = []
   
 with open("./czech-words.txt", "r", encoding="utf8") as file:
     slova = file.read().splitlines()
 
-print(random.choice(slova))
+slovo = print(random.choice(slova))
+
+
+
+pismeno = input("Jaké písmeno hádáš: ")
